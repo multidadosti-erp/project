806104a4ff367b6939bbf527a2d56c9457063050
Augusto Costa <gustotc@gmail.com>
2023-06-30 09:46:32 -0300

    Ajusta módulo 'project_stage_state' para rodar com MultiERP

--- project_stage_state/models/project_task_type.py
+++ project_stage_state/models/project_task_type.py
@@ -1,7 +1,7 @@
 # Copyright 2014 Daniel Reis
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 _TASK_STATE = [
@@ -17,4 +17,19 @@ class ProjectTaskType(models.Model):
 
     _inherit = 'project.task.type'
 
-    state = fields.Selection(_TASK_STATE, 'State')
+    state = fields.Selection(
+        _TASK_STATE,
+        'State'
+    )
+
+    closed = fields.Boolean(
+        help="Tasks in this stage are considered closed.",
+        compute='_compute_closed',
+        store=True,
+    )
+
+    @api.multi
+    @api.depends('state')
+    def _compute_closed(self):
+        for stage in self:
+            stage.closed = stage.state in ('done', 'cancelled')
