1d740fc01a822ab28ca0159cfbb2e22ab344270a
Augusto Costa <gustotc@gmail.com>
2023-07-03 13:47:15 -0300

    Ajusta Criação do Projeto com Estágios Padrão módulo 'project_task_default_stage'

--- project_task_default_stage/models/project.py
+++ project_task_default_stage/models/project.py
@@ -1,7 +1,7 @@
 # Copyright 2015 Incaser Informatica S.L. - Sergio Teruel
 # Copyright 2015 Incaser Informatica S.L. - Carlos Dauden
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class ProjectProject(models.Model):
@@ -12,5 +12,20 @@ class ProjectProject(models.Model):
             ('case_default', '=', True)])
         return ids
 
-    type_ids = fields.Many2many(
-        default=lambda self: self._get_default_type_common())
+    @api.model
+    def create(self, values):
+        """ Metodo para adicionar os estágios padrão no projeto.
+
+        Arguments:
+            values {dict} -- valores para criação do projeto.
+
+        Returns:
+            project.project -- retorna criação do projeto
+        """
+        if 'type_ids' not in values:
+            standard_stage_ids = self._get_default_type_common()
+
+            if standard_stage_ids:
+                values['type_ids'] = [(6, 0, standard_stage_ids.ids)]
+
+        return super(ProjectProject, self).create(values)
